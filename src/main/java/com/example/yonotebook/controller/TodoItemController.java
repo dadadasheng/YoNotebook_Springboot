05c2c1f0ed37427295d1de84b666700d8797fbac
Chern <781327871@qq.com>
2025-08-03 21:46:11 +0800

    some api

--- src/main/java/com/example/yonotebook/controller/TodoItemController.java
+++ src/main/java/com/example/yonotebook/controller/TodoItemController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 @CrossOrigin("*")
@@ -18,14 +20,15 @@ public class TodoItemController {
     private TodoItemRepository todoItemRepository;
 
     @PostMapping(path = "/add")
-    public @ResponseBody String addTodoItem(@RequestParam String todo_text, @RequestParam int status) {
+    public @ResponseBody TodoItem addTodoItem(@RequestParam String todo_text, @RequestParam int status, @RequestParam int year, @RequestParam int month, @RequestParam int day) {
         TodoItem todoItem = new TodoItem();
         todoItem.setTodoText(todo_text);
         todoItem.setStatus(status);
-        Date date = new Date();
+        LocalDate localDate = LocalDate.of(year, month, day);
+        Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
         todoItem.setcreateTime(date);
         todoItemRepository.save(todoItem);
-        return "save";
+        return todoItem;
     }
     //edit todo item
     //del todo item
@@ -35,12 +38,15 @@ public class TodoItemController {
         return todoItemRepository.findAll();
     }
     @GetMapping(path = "/create_time")
-    public @ResponseBody Iterable<TodoItem> getAllTodoItemsByCreateTime(@RequestParam String year, @RequestParam String month, @RequestParam String day) throws ParseException {
-        Date d = new Date();
-        return todoItemRepository.findTodoItemsByCreateTimeBetween(new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(year+"-"+month+"-"+day+" 00:00:00.000000"),new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(year+"-"+month+"-"+(Integer.parseInt(day)+1)+" 00:00:00.000000"));
-    }
+    public @ResponseBody Iterable<TodoItem> getAllTodoItemsByCreateTime(@RequestParam int year, @RequestParam int month, @RequestParam int day) throws ParseException {
+        String startStr = String.format("%04d-%02d-%02d 00:00:00.000", year, month, day);
+        String endStr = String.format("%04d-%02d-%02d 00:00:00.000", year, month, day + 1);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date start = sdf.parse(startStr);
+        Date end = sdf.parse(endStr);
+        return todoItemRepository.findTodoItemsByCreateTimeBetween(start, end);}
 
-    @GetMapping(path="/updateToDoItemContent")
+    @PostMapping(path="/updateToDoItemContent")
     public @ResponseBody String updateToDoItemContent(@RequestParam String todo_text, @RequestParam int Id) {
         TodoItem todoItem = todoItemRepository.findById(Id).orElseThrow();
         todoItem.setTodoText(todo_text);
@@ -48,11 +54,12 @@ public class TodoItemController {
         return "update content";
     }
 
-    @GetMapping(path="/updateToDoItemStatus")
+    @PostMapping(path="/updateToDoItemStatus")
     public @ResponseBody String updateToDoItemStatus(@RequestParam int status, @RequestParam int Id) {
         TodoItem todoItem = todoItemRepository.findById(Id).orElseThrow();
         todoItem.setStatus(status);
         todoItemRepository.save(todoItem);
         return "update status";
     }
+
 }
